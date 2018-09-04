7e807a52795865daadb3d084953617d69514ae7f
Makoto Ito <3mdevmail@gmail.com>
2018-09-04 20:33:42 +0900

    :shirt: ESLintの設定を少し変更

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "parserOptions": {
+    "parser": "babel-eslint"
+  },
+  "extends": [
+    "plugin:vue/recommended",
+    "plugin:vue/essential"
+  ],
+  // required to lint *.vue files
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  }
+}
