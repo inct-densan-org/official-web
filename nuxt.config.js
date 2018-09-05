f4c20f280f563665c9673472ffeef48793df2f58
Makoto Ito <3mdevmail@gmail.com>
2018-09-05 22:53:13 +0900

    :sparkles: favicon等追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,27 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'official-web',
+    title: '一関高専 電子計算機部',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '独立行政法人国立高等専門学校機構一関工業高等専門学校 電子計算機部オフィシャルウェブ' }
+      { hid: 'description', name: 'description', content: '独立行政法人国立高等専門学校機構一関工業高等専門学校 電子計算機部オフィシャルウェブ' },
+      { hid: 'og:description', name: 'og:description', content: '独立行政法人国立高等専門学校機構一関工業高等専門学校 電子計算機部オフィシャルウェブ' },
+      { hid: 'og:type', name: 'og:type', content: 'website' },
+      { hid: 'og:url', name: 'og:url', content: 'https://inct-densan.club' },
+      { hid: 'og:image', name: 'og:image', content: 'https://media.inct-densan.club/site/densan-circle-logo.png' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@inct_densan' },
+      { hid: 'twitter:creator', name: 'twitter:creator', content: '@inct_densan' },
+      { hid: 'msapplication-TileColor', name: 'msapplication-TileColor', content: '#ffffff' },
+      { hid: 'theme-color', name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
     ]
   },
   css: [
