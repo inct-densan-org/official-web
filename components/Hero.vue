116c06ee805c467dc7a7139d1d0eca30695729c1
Makoto Ito <3mdevmail@gmail.com>
2018-09-05 19:44:20 +0900

    :+1: 名前的にHTMLタグとかぶって動かなかったので修正

---
+++ components/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hero is-success is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <densan-logo/>
+        <div class="contents">
+          <h1 class="title is-1">
+            一関高専 電子計算機部
+          </h1>
+          <h2 class="subtitle is-3">
+            指先から、世界を変える。
+          </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DensanLogo from '~/components/DensanLogo.vue'
+
+export default {
+  components: {
+    DensanLogo
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.contents
+  padding-bottom 30px
+</style>
