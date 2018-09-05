c29e1ce6f631262bf8e7d33b846d5ccb867d1aee
Makoto Ito <3mdevmail@gmail.com>
2018-09-05 21:54:12 +0900

    :sparkles: プライバシーポリシー追加

---
+++ pages/privacy.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="contents">
+    <div class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Privacy
+          </h1>
+          <h2 class="subtitle">
+            プライバシーポリシー
+          </h2>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="top-space ">
+        <p>
+          当サイトでは、サイト向上のために、 Google Inc. が提供するアクセス解析ツール「Google アナリティクス」を利用しています。<br>
+          この Google アナリティクスは、トラフィックデータ収集のために Cookie を使用しています。<br>
+          このトラフィックデータは匿名で収集されており、個人を特定するものではありません。<br>
+          詳しくは、「
+          <a
+            href="https://policies.google.com/technologies/partner-sites?hl=ja"
+            target="_blank">
+            ユーザーが Google パートナーのサイトやアプリを使用する際の Google によるデータ使用 – ポリシーと規約 – Google
+            <sup><i class="fas fa-fw fa-external-link-alt"/></sup>
+          </a>」をご覧ください。<br>
+          また、この機能は Cookie を無効にすることで、データの提供を拒否することができますので、お使いのブラウザの設定をご確認ください。
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.contents
+  margin-bottom 30px
+
+.top-space
+  margin-top 20px
+</style>
