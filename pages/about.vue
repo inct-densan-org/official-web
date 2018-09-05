52327563215947237353a690d105e5b24c955f77
Makoto Ito <3mdevmail@gmail.com>
2018-09-05 19:45:49 +0900

    :sparkles: Aboutページ追加

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            About
+          </h1>
+          <h2 class="subtitle">
+            電算部の活動について
+          </h2>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="contents">
+        <h1 class="title is-1">電算部とは…?</h1>
+        <p>電算部とは、一関高専の部活動の1つであり、正式名称は「電子計算機部」です。</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.contents
+  margin-top 30px
+</style>
