8429ecae5a3a42cc165ee6a247bf0b1f2f1d4980
Makoto Ito <3mdevmail@gmail.com>
2018-09-06 21:33:50 +0900

    :sparkles: Skillsページを追加

---
+++ pages/skills.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="contents">
+    <div class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Skills
+          </h1>
+          <h2 class="subtitle">
+            私達が扱える技術など
+          </h2>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div>
+        <h2 class="title is-3">言語</h2>
+        <ul>
+          <li>C</li>
+          <li>C++</li>
+          <li>C#</li>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>Python</li>
+          <li>Java(Kotlin)</li>
+          <li>Ruby</li>
+        </ul>
+        <h2 class="title is-3">フレームワーク・技術</h2>
+        <ul>
+          <li>Web</li>
+          <ul>
+            <li>Vue.js(Nuxt.js)</li>
+            <li>Node Express</li>
+            <li>Django</li>
+            <li>Ruby on Rails</li>
+            <li>WebRTC</li>
+            <li>Bootstrap</li>
+            <li>Bulma</li>
+          </ul>
+        </ul>
+        <ul>
+          <li>Game</li>
+          <ul>
+            <li>Unity</li>
+            <li>DXライブラリ</li>
+          </ul>
+        </ul>
+        <ul>
+          <li>Infrastructure</li>
+          <ul>
+            <li>Microsoft Azure</li>
+            <li>Ubuntu</li>
+            <li>CentOS</li>
+            <li>Arch Linux</li>
+            <li>Docker</li>
+            <li>IOS(Cisco)</li>
+          </ul>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.hero
+  margin-top 3.23rem
+
+.contents
+  margin-bottom 30px
+
+.title
+  margin-top 20px
+
+ul
+  list-style disc
+  padding-left 1rem
+</style>
